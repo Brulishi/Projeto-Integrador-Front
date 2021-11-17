320a25c0be24d874efad46fdfd9b67dc9e065842
Pablo <bruli_pablo@hotmail.com>
2021-11-17 18:45:46 -0300

    [Feature Pablo - Task: Tentativa de login e cadastro versão2]

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
 import { CredenciaisDTO } from '../model/CredenciaisDTO';
+import { Usuario } from '../model/Usuario';
 import { AuthService } from '../service/auth.service';
 
 @Component({
@@ -10,7 +11,12 @@ import { AuthService } from '../service/auth.service';
   styleUrls: ['./login.component.css'],
 })
 export class EntrarComponent implements OnInit {
+  usuario: Usuario = new Usuario();
+  confirmarSenha: string;
+  tipoUsuario: string;
+
   usuarioLogin: CredenciaisDTO = new CredenciaisDTO();
+  authService: any;
 
   constructor(private auth: AuthService, private router: Router) {}
 
@@ -19,6 +25,33 @@ export class EntrarComponent implements OnInit {
 
     environment.token = ''; // Sempre que voltar para a pagina entrar, o token zera e o usuario deve fazer login novamente
   }
+
+  confirmSenha(event: any) {
+    this.confirmarSenha = event.target.value;
+  }
+
+  tipoUser(event: any) {
+    this.tipoUsuario = event.target.value;
+  }
+
+  cadastrar() {
+    if (this.confirmarSenha.length < 8) {
+      alert('A senha deve ter no minimo 8 caracteres!');
+    } else {
+      this.usuario.tipo = this.tipoUsuario;
+
+      if (this.usuario.senha != this.confirmarSenha) {
+        alert('As senhas estão diferentes!');
+      } else {
+        this.authService.cadastrar(this.usuario).subscribe((resp: Usuario) => {
+          this.usuario = resp;
+          this.router.navigate(['/login']);
+          alert('Cadastro realizado com sucesso!');
+        });
+      }
+    }
+  }
+
 
   entrar() {
     this.auth.entrar(this.usuarioLogin).subscribe(
@@ -37,6 +70,7 @@ export class EntrarComponent implements OnInit {
           environment.foto = this.usuarioLogin.foto;
         }
 
+        
         // console.log(environment.token)
         // console.log(environment.id)
         // console.log(environment.nome)
